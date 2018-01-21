5648b398c279d68407dab24e7e63cbe0c953f8fd
Andrew Kennedy <akennedy2292@gmail.com>
2018-01-21 15:02:30 -0800

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const Root = () => {
+  return (
+    <div>hello</div>
+  )
+}
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+)
